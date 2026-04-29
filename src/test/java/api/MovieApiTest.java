06f01df1e993aa94f271448bb835562d11c15e43
Avner01 <chl.avner@gmail.com>
2026-04-29 15:01:00 +0200

    feat: add authentication and filter tests

--- src/test/java/api/MovieApiTest.java
+++ src/test/java/api/MovieApiTest.java
@@ -86,4 +86,54 @@ public class MovieApiTest extends BaseTest {
 
         System.out.println("Invalid API Key test passed!");
     }
+
+    @Test
+    public void testGetMovieWithHeaders() {
+        Response response = RestAssured
+                .given()
+                .header("Authorization", "Bearer " + ConfigReader.get("tmdb.api.key"))
+                .header("Content-Type", "application/json")
+                .when()
+                .get("/movie/popular")
+                .then()
+                .statusCode(200)
+                .extract()
+                .response();
+
+        Assert.assertNotNull(response.jsonPath().getList("results"));
+        System.out.println("Headers Test Passed!");
+    }
+
+    @Test
+    public void testFilterMoviesByGenre() {
+        Response response = RestAssured
+                .given()
+                .queryParam("api_key", ConfigReader.get("tmdb.api.key"))
+                .queryParam("with_genres", "28")
+                .when()
+                .get("/discover/movie")
+                .then()
+                .statusCode(200)
+                .extract()
+                .response();
+
+        Assert.assertTrue(response.jsonPath().getList("results").size() > 0);
+        System.out.println("Action Movies: " + response.jsonPath().getList("results.title"));
+    }
+
+    @Test
+    public void testGetTopRatedMovies() {
+        Response response = RestAssured
+                .given()
+                .queryParam("api_key", ConfigReader.get("tmdb.api.key"))
+                .when()
+                .get("/movie/top_rated")
+                .then()
+                .statusCode(200)
+                .extract()
+                .response();
+
+        Assert.assertNotNull(response.jsonPath().getList("results"));
+        System.out.println("Top Rated: " + response.jsonPath().getList("results.title"));
+    }
 }
